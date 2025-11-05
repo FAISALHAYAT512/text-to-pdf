729c16180bddf6bde1ce7aa5b35b3e913e9bef63
FAISALHAYAT512 <faisalhayath98@gmail.com>
2025-11-05 14:48:21 +0500

    text-to-pdf

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true, // React compiler enabled
-  experimental: {
-    turbopack: false, // ✅ Turbopack temporarily disabled (build error fix)
-  },
   // Optional: Future proof settings
   compiler: {
     styledComponents: true, // Agar aap styled-components use karte ho
